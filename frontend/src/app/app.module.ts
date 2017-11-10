ad230bc5fd7dbac2bab4ec65afebfe61b55705ee
Maksim Pliusch <mpliusch@exadel.com>
2017-11-10 17:14:16 +0300

    First step

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/customers/mock-customers.ts
@@ -0,0 +1,15 @@
+import { Customer } from './customer'
+
+export const CUSTOMERS: Customer[] = [{
+    id: 1,
+    name: 'Maksim Pliusch'
+}, {
+    id: 2,
+    name: 'Anastasiya Plahina'
+}, {
+    id: 3,
+    name: 'Shmidt Vasily'
+}, {
+    id: 4,
+    name: 'Ngo-Ma'
+}];
